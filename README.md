21e3029741c5bb46efb974d29e97726274fbacd8
Bruno Machado Brandão <brunin5010@gmail.com>
2018-01-17 11:50:18 -0200

    Incluindo arquivos de configuração e criação do README

---
+++ README.md
@@ -0,0 +1,64 @@
+ETL
+===============
+
+ETL é um *bitcode* de processamento e transformação de arquivos (ETL) para [Thrust](https://github.com/Thrustjs/thrust).
+
+## Tutorial
+
+```javascript
+let etl = require('etl')
+let file = new java.io.File('planilha.xlsx').getAbsolutePath()
+
+  etl({})
+    .set_input_xlsx_file(file, 0)
+    .take_fields_from_header_row()
+    .for_each(function (options, values) {
+      show('CPF: ', values.C)
+    })
+
+```
+
+## API
+
+```javascript
+/*
+* Construtor do etl com um objeto de configurações de
+* processamento.
+*/
+let etl = etl(options)
+
+/*
+ * 'etl' terá as seguintes funções, utilizadas para
+ * setar a fonte de dados que será processada
+*/
+set_input_text_file(textFile)
+set_input_xlsx_file(xlsxFile, sheetNumber)
+set_input_string(content)
+
+/*
+ * Os métodos acima retornarão:
+*/
+
+/*
+ * Preenche a propriedade fields no objeto de options, com todos os campos do header do arquivo
+*/
+take_fields_from_header_row()
+
+/*
+ * Função a ser executada antes de iniciar o loop das linhas
+ * @param {Function} fn Função com assinatura (options)
+*/
+before_for_each(fn)
+
+/*
+ * Função a ser executada para cada linha
+ * @param {Function} fn Função com assinatura (options, values)
+*/
+for_each(fn)
+
+/*
+ * Função a ser executada após a finalização do loop das linhas
+ * @param {Function} fn Função com assinatura (options)
+*/
+after_for_each(fn)
+```
--- dist/index.js
+++ dist/index.js
@@ -3,90 +3,90 @@ var XlsxOutputStream = require("./xlsx_stream")
 var CsvOutputStream = require("./csv_stream")
 
 function tokenizer(options) {
-    return { 
-        set_input_text_file: set_input_text_file.bind(null, options),
-        set_input_xlsx_file: set_input_xlsx_file.bind(null, options),
-        set_input_string: set_input_string.bind(null, options)
-    }
+  return {
+    set_input_text_file: set_input_text_file.bind(null, options),
+    set_input_xlsx_file: set_input_xlsx_file.bind(null, options),
+    set_input_string: set_input_string.bind(null, options)
+  }
 }
 
 tokenizer.counter = function _counter(initial_value) {
-    var nextNumber = initial_value || 0
+  var nextNumber = initial_value || 0
 
-    return {
-        next: function () {
-            return nextNumber < Infinity ?
-                { value: nextNumber++, done: false } :
-                { done: true }
-        }
+  return {
+    next: function () {
+      return nextNumber < Infinity ?
+        { value: nextNumber++, done: false } :
+        { done: true }
     }
+  }
 }
 
 function set_input_text_file(options, text_file) {
-    var stream = new CsvOutputStream(text_file, options)
+  var stream = new CsvOutputStream(text_file, options)
 
-    return { 
-        take_fields_from_header_row: take_fields_from_header_row.bind(null, stream, options),
-        before_for_each: before_for_each.bind(null, stream, options),
-        for_each: for_each.bind(null, stream, options)
-    }
+  return {
+    take_fields_from_header_row: take_fields_from_header_row.bind(null, stream, options),
+    before_for_each: before_for_each.bind(null, stream, options),
+    for_each: for_each.bind(null, stream, options)
+  }
 }
 
 function set_input_xlsx_file(options, xlsx_file, sheet) {
-    var stream = new XlsxOutputStream(xlsx_file, sheet, options)
+  var stream = new XlsxOutputStream(xlsx_file, sheet, options)
 
-    return { 
-        take_fields_from_header_row: take_fields_from_header_row.bind(null, stream, options),
-        before_for_each: before_for_each.bind(null, stream, options),
-        for_each: for_each.bind(null, stream, options)
-    }
+  return {
+    take_fields_from_header_row: take_fields_from_header_row.bind(null, stream, options),
+    before_for_each: before_for_each.bind(null, stream, options),
+    for_each: for_each.bind(null, stream, options)
+  }
 }
 
 function set_input_string(options, content) {
-    // var stream = JArrays.stream(content.split(options.rowsep))
-    var stream = JArrays.stream(Java.to(content.split(options.rowsep), Java.type('java.lang.String[]')))
+  // var stream = JArrays.stream(content.split(options.rowsep))
+  var stream = JArrays.stream(Java.to(content.split(options.rowsep), Java.type('java.lang.String[]')))
 
-    return { 
-        take_fields_from_header_row: take_fields_from_header_row.bind(null, stream, options),
-        before_for_each: before_for_each.bind(null, stream, options),
-        for_each: for_each.bind(null, stream, options)
-    }
+  return {
+    take_fields_from_header_row: take_fields_from_header_row.bind(null, stream, options),
+    before_for_each: before_for_each.bind(null, stream, options),
+    for_each: for_each.bind(null, stream, options)
+  }
 }
 
 function take_fields_from_header_row(stream, options) {
 
-    var fields = stream.take_fields_from_header_row()
-    var tail = stream.take_tail()
-    var opts = Object.assign({}, options) 
-    
-    opts.fields = fields
+  var fields = stream.take_fields_from_header_row()
+  var tail = stream.take_tail()
+  var opts = Object.assign({}, options)
 
-    return {
-        before_for_each: before_for_each.bind(null, tail, opts),
-        for_each: for_each.bind(null, tail, opts)
-    }
+  opts.fields = fields
+
+  return {
+    before_for_each: before_for_each.bind(null, tail, opts),
+    for_each: for_each.bind(null, tail, opts)
+  }
 }
 
 function before_for_each(stream, options, fnc) {
-    options.rowid = tokenizer.counter()
-    
-    fnc(options)
+  options.rowid = tokenizer.counter()
 
-    return {
-        for_each: for_each.bind(null, stream, options)
-    }    
+  fnc(options)
+
+  return {
+    for_each: for_each.bind(null, stream, options)
+  }
 }
 
 function after_for_each(options, fnc) {
-    fnc(options)  
+  fnc(options)
 }
 
 function for_each(stream, options, fnc) {
-    stream.forEach( fnc.bind(null, options) )
- 
-    return {
-        after_for_each: after_for_each.bind(null, options)
-    }       
+  stream.forEach(fnc.bind(null, options))
+
+  return {
+    after_for_each: after_for_each.bind(null, options)
+  }
 }
 
 exports = tokenizer
